b7864e209a1549f1d1cef633930ffe4d2d29977a
Vinayakumar B <vinayakumarb@apache.org>
2020-08-20 14:33:33 +0530

    Support proxy users for hive beeline

--- compose/non-security-non-ha/hive/utils/initAndStartMetastore.sh
+++ compose/non-security-non-ha/hive/utils/initAndStartMetastore.sh
@@ -2,9 +2,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
 cp $HADOOP_HOME/share/hadoop/client/*.jar $HIVE_HOME/lib
-#Init the schema
-schematool -dbType derby -initSchema
-#Create the basic dirs
-hdfs dfs -mkdir -p /user/hive/warehouse
+if ![ -f /tmp/${USER}/HIVE_INITIALIZED ]; then
+  #Init the schema
+  schematool -dbType derby -initSchema
+  #Create the basic dirs
+  hdfs dfs -mkdir -p /user/hive/warehouse
+  touch /tmp/${USER}/HIVE_INITIALIZED;
+fi
 #Start metastore
-hive --service metastore
+nohup hive --service metastore > /tmp/${USER}/hivemeta.log &
+hive --service hiveserver2
